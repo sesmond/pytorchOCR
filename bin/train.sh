f5c62acbb4c5c092197892b215d86fc921e55459
vincent <>
2021-11-03 13:50:08 +0800

    up

--- bin/train.sh
+++ bin/train.sh
@@ -3,6 +3,7 @@ select var in "检测" "识别"; do
   break;
 done
 echo "您选择了： $var  "
+Date=$(date +%Y%m%d%H%M)
 
 if [ "$var" = "检测" ]; then
     select dnet in "DB" "PSE" "PAN" "SAST"; do
@@ -36,14 +37,19 @@ if [ "$var" = "检测" ]; then
     esac
     config_file="./config/det_${dnet}_${dbackbone}.yaml"
     echo "配置文件："$config_file
+    read -p "输入训练要用的GPU:" gpus ;
+    echo "您选择了GPU $gpus 进行训练"
     set -x
+    nohup \
     python -m tools.det_train \
       --config $config_file \
       --log_str "logs" \
       --n_epoch 1200 \
       --start_val 600 \
       --base_lr 0.002 \
-      --gpu_id 2
+      --gpu_id $gpus \
+       >> ./logs/det_train_$Date.log 2>&1 &
+    echo "启动完毕,在logs下查看日志！"
     exit
 fi
 
