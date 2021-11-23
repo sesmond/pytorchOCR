757d8fa602e07a4931c55f3bfd32684d60e027eb
vincent <>
2021-11-23 16:12:28 +0800

    bugfix

--- ptocr/utils/transform_label.py
+++ ptocr/utils/transform_label.py
@@ -39,11 +39,11 @@ class StrLabelConverter(object):
         alphabet = get_keys(config['trainload']['key_file'])
         # TODO 空格用'～'表示，预测的时候也用'～'解析，解析完之后替换
         self.alphabet = alphabet + "～"  # for `-1` index
-        self.dict = {}
+        self.dict_index = {}
         # TODO 给字符排序 TODO 这里预测的时候也必须一样才能预测，不然没法预测
         for i, char in enumerate(alphabet):
             # NOTE: 0 is reserved for 'blank' required by wrap_ctc  TODO 空格是0，所以用的i+1
-            self.dict[char] = i + 1
+            self.dict_index[char] = i + 1
 
     def encode(self, text, t_step):
         """Support batch or single str.
@@ -63,10 +63,9 @@ class StrLabelConverter(object):
             line = text[i]
             line = line.replace(" ", "～")
             for j in range(len(line)):
-                print(line, dict)
-                index = self.dict[line[j]]
+                print(line, self.dict_index)
+                index = self.dict_index[line[j]]
                 result.append(index)
-
         text = result
         return (torch.IntTensor(text), torch.IntTensor(length))
 
